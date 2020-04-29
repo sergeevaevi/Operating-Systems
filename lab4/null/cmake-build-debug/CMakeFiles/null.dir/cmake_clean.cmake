423ce6a70ba520e37b6b7bc566bd424bb6bcc6f0
elen <34635421+sergeevaevi@users.noreply.github.com>
2020-04-29 14:18:34 +1000

    Add files via upload

---
+++ lab4/null/cmake-build-debug/CMakeFiles/null.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/null.dir/main.c.o"
+  "null"
+  "null.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/null.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab4/null/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int * p = NULL;
+    printf("It is emptiness %d\n", *p);
+    return 0;
+}
