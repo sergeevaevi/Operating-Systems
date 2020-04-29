ac4b1a4849f38de7c821b67463a7c078ea7c7fc4
elen <34635421+sergeevaevi@users.noreply.github.com>
2020-04-29 14:24:20 +1000

    Add files via upload

---
+++ lab4/malloc/cmake-build-debug/CMakeFiles/malloc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/malloc.dir/main.c.o"
+  "malloc"
+  "malloc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/malloc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab4/malloc/main.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+int main() {
+    void* p = malloc(64);
+    return 0;
+}
