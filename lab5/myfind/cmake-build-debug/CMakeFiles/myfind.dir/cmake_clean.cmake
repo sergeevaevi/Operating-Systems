ea03b4515c1bacca64640894481143ff6a016235
elen <34635421+sergeevaevi@users.noreply.github.com>
2020-06-04 14:08:59 +1000

    Add files via upload

---
+++ lab5/myfind/cmake-build-debug/CMakeFiles/myfind.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myfind.dir/main.c.o"
+  "myfind"
+  "myfind.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myfind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
